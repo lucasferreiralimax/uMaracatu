d655995150ccad7fbd2f00f713b043bcb8dda39b
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-07-18 03:10:41 -0300

    test(unit): clean spec of app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,12 +14,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'uMaracatu' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('uMaracatu');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
